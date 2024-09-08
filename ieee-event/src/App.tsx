6b90e9dd114ce4e100193040b245ddc72163e284
alexis <am9517534@gmail.com>
2024-09-08 17:38:03 -0500

    Añadiendo que el card del inicio sea reutilizable

--- ieee-event/src/App.tsx
+++ ieee-event/src/App.tsx
@@ -1,17 +1,37 @@
 
 import './App.css'
 import NavBar from "./components/header.tsx";
-import Home from "./components/home.tsx";
+import PanelWithImg from "./components/panelWithImg.tsx";
 
 
 function App() {
 
 
   return (
-  <>
-      <NavBar />
-      <Home></Home>
-  </>
+      <>
+          <NavBar/>
+          <div>
+              <PanelWithImg
+                  imageUrl="https://www.vipnet360.com/wp-content/uploads/2021/06/La-importancia-de-contar-con-un-desarrollador-o-programador-para-Wordpress_Vipnet360.png"
+                  title="TALLER: INTRODUCCION A R"
+                  description="De Funciones a Paquetes, de Paquetes a Papers"
+                  buttonText="Registrarme"
+                  buttonHref="https://www.gaaaaaaa.com"
+              />
+          </div>
+
+
+          <div>
+              <PanelWithImg
+                  imageUrl="https://www.profesionalonline.com/blog/wp-content/uploads/2022/01/los-7-tipos-de-programador-explicados-header.jpg"
+                  title="EVENTO VIRTUAL"
+                  description="________________________________________________"
+                  buttonText="Registrarme"
+                  buttonHref="https://www.gaaaaaaa.com"
+              />
+          </div>
+
+      </>
   )
 }
 
